df1bbae77ae5b45cc5477c82a012fe8677e73427
H.Vasev <hristovasev999@gmail.com>
2024-02-24 09:10:32 +0000

    Sort Usings

--- RestaurantApp.Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ RestaurantApp.Infrastructure/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,9 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using RestaurantApp.Data;
-using System;
 
 namespace RestaurantApp.Data.Migrations
 {
