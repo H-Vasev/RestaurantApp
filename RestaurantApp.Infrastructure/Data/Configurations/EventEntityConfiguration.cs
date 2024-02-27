2d2064839686c5649bf00629f628ba81cd9263e2
H.Vasev <hristovasev999@gmail.com>
2024-02-27 18:05:53 +0000

    SeedEvents

---
+++ RestaurantApp.Infrastructure/Data/Configurations/EventEntityConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantApp.Infrastructure.Data.Models;
+
+namespace RestaurantApp.Infrastructure.Data.Configurations
+{
+	public class EventEntityConfiguration : IEntityTypeConfiguration<Event>
+	{
+		public void Configure(EntityTypeBuilder<Event> builder)
+		{
+			builder.HasData(GenerateEvents());
+		}
+
+		private ICollection<Event> GenerateEvents()
+		{
+			return new Event[]
+			{
+				new Event
+				{
+					Id = 1,
+					Title = "Christmas Day",
+					Description = "Christmas Day",
+					StartEvent = DateTime.Parse("25.12.2024 20:00"),
+					EndEvent = DateTime.Parse("25.12.2024 12:00")
+				},
+				new Event
+				{
+					Id = 2,
+					Title = "Heppy New Year",
+					Description = "Heppy New Year",
+					StartEvent = DateTime.Parse("31.12.2024 20:00"),
+					EndEvent = DateTime.Parse("31.12.2024 12:00")
+				},
+				new Event
+				{
+					Id = 3,
+					Title = "Easter Sunday",
+					Description = "Easter Sunday",
+					StartEvent = DateTime.Parse("31.03.2024 20:00"),
+					EndEvent = DateTime.Parse("31.03.2024 12:00")
+				},
+			};
+		}
+	}
+}
---
+++ RestaurantApp.Infrastructure/Migrations/20240227180434_SeedEvents.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantApp.Infrastructure.Migrations
+{
+    public partial class SeedEvents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Description", "EndEvent", "StartEvent", "Title" },
+                values: new object[] { 1, "Christmas Day", new DateTime(2024, 12, 25, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 25, 20, 0, 0, 0, DateTimeKind.Unspecified), "Christmas Day" });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Description", "EndEvent", "StartEvent", "Title" },
+                values: new object[] { 2, "Heppy New Year", new DateTime(2024, 12, 31, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 12, 31, 20, 0, 0, 0, DateTimeKind.Unspecified), "Heppy New Year" });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Description", "EndEvent", "StartEvent", "Title" },
+                values: new object[] { 3, "Easter Sunday", new DateTime(2024, 3, 31, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2024, 3, 31, 20, 0, 0, 0, DateTimeKind.Unspecified), "Easter Sunday" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
