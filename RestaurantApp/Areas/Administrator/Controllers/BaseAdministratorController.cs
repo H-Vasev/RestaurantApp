eaca0093b21be183a03493758a3800bfb69eafed
H.Vasev <hristovasev999@gmail.com>
2024-03-02 16:00:21 +0000

    Add Administrator area

---
+++ RestaurantApp/Areas/Administrator/Controllers/BaseAdministratorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantApp.Areas.Administrator.Controllers
+{
+	[Authorize(Roles = "Administrator")]
+	[Area("Administrator")]
+	public class BaseAdministratorController : Controller
+	{
+	}
+}
