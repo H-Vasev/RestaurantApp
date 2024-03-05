ed15e7ef7fc5c17a598f16a77dff24e61be839dc
H.Vasev <hristovasev999@gmail.com>
2024-03-05 21:43:10 +0000

    Implement get all images form gallery

---
+++ RestaurantApp/Controllers/GalleryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantApp.Core.Contracts;
+
+namespace RestaurantApp.Controllers
+{
+    public class GalleryController : BaseController
+    {
+        private readonly IGalleryService galleryService;
+
+        public GalleryController(IGalleryService galleryService)
+        {
+            this.galleryService = galleryService;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            var model = await galleryService.GetAllGalleryImagesAsync();
+
+            return View(model);
+        }
+    }
+}
