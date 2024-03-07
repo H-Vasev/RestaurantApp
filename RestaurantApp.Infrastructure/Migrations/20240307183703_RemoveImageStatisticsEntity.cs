bf2fddffd27def1b2efc54a56162da74536e79b4
H.Vasev <hristovasev999@gmail.com>
2024-03-07 18:57:06 +0000

    Remove ImageStatistics

---
+++ RestaurantApp.Infrastructure/Migrations/20240307183703_RemoveImageStatisticsEntity.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantApp.Infrastructure.Migrations
+{
+    public partial class RemoveImageStatisticsEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageStatistics");
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8842));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 6, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8848));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 5, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8850));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 4, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8852));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 3, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8853));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 2, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8855));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 1, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8857));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedOn",
+                value: new DateTime(2024, 2, 29, 18, 37, 2, 549, DateTimeKind.Local).AddTicks(8858));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImageStatistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GalleryImageId = table.Column<int>(type: "int", nullable: false),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    Views = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageStatistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ImageStatistics_GalleryImages_GalleryImageId",
+                        column: x => x.GalleryImageId,
+                        principalTable: "GalleryImages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 5, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2168));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 4, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2172));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 3, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2175));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 2, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2176));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 1, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2178));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedOn",
+                value: new DateTime(2024, 2, 29, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2180));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedOn",
+                value: new DateTime(2024, 2, 28, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2181));
+
+            migrationBuilder.UpdateData(
+                table: "GalleryImages",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "CreatedOn",
+                value: new DateTime(2024, 2, 27, 21, 6, 42, 889, DateTimeKind.Local).AddTicks(2183));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageStatistics_GalleryImageId",
+                table: "ImageStatistics",
+                column: "GalleryImageId",
+                unique: true);
+        }
+    }
+}
