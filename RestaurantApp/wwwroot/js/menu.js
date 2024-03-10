f9b52687c4cd1b264409cb45e3e08afcd3309281
H.Vasev <hristovasev999@gmail.com>
2024-03-10 21:28:52 +0000

    Add js file and implement jquery for adding products

---
+++ RestaurantApp/wwwroot/js/menu.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function () {
+    $('.add-to-cart').click(function (e) {
+        e.preventDefault();
+
+        var productId = $(this).data('id');
+        console.log(productId)
+        $.ajax({
+            url: '/Menu/AddToCart',
+            type: 'POST',
+            data: {
+                id: productId,
+                __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val()
+            },
+            success: function (response) {
+
+                updateCartCount();
+
+                var messageHtml = '<div id="successResult" class="customAlertMessage alert alert-success text-center mt-5" > Product added to basket successfully! </div>';
+                $('#message-container').html(messageHtml);
+
+                setTimeout(function () {
+                    $('#successResult').fadeOut('slow', function () {
+                        $(this).remove();
+                    });
+                }, 3000);
+            },
+            error: function (error) {
+                console.error("Error adding to cart: ", error);
+            }
+        });
+
+        function updateCartCount() {
+            $.ajax({
+                url: '/ShoppingCart/GetCartItemsCount',
+                type: 'GET',
+                success: function (response) {
+                    $('#cartItemCount').text(response.count);
+                },
+                error: function (error) {
+                    console.error("Error updating cart count: ", error);
+                }
+            });
+        }
+    });
+});
