1aa97edc91a61ad52db179ae54a9f2e613927b5c
H.Vasev <hristovasev999@gmail.com>
2024-03-10 16:31:40 +0000

    Implement IncrementImageCount

--- RestaurantApp/Controllers/GalleryController.cs
+++ RestaurantApp/Controllers/GalleryController.cs
@@ -20,5 +20,37 @@ namespace RestaurantApp.Controllers
 
             return View(model);
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> IncrementImageCount(int id)
+        {
+            var cookieName = "IncrementedImages";
+            var incrementedImages = new List<int>();
+            var isIdExist = true;
+
+            if (Request.Cookies.ContainsKey(cookieName))
+            {
+                var info = Request.Cookies[cookieName];
+				incrementedImages = Request.Cookies[cookieName].Split(',').Select(int.Parse).ToList();
+			}
+
+            if (!incrementedImages.Contains(id))
+            {
+				incrementedImages.Add(id);
+                var cookieOptions = new CookieOptions 
+                { 
+                    Expires = DateTimeOffset.Now.AddDays(30), 
+                    HttpOnly = true 
+                };
+				Response.Cookies.Append(cookieName, string.Join(',', incrementedImages), cookieOptions);
+                isIdExist = false;
+			}
+
+            var imageCount = await galleryService.IncrementImageViewsCountAsync(id, isIdExist);
+
+            return Json(new {viewCount = imageCount});
+        }
     }
 }
