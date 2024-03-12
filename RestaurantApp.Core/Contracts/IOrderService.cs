bd757c03fcb9b5e20e450fcda66c43cb61ce622a
H.Vasev <hristovasev999@gmail.com>
2024-03-12 21:21:19 +0000

    Implement checkOut

---
+++ RestaurantApp.Core/Contracts/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using RestaurantApp.Core.Models.ShoppingCart;
+
+namespace RestaurantApp.Core.Contracts
+{
+	public interface IOrderService
+	{
+		Task CheckoutAsync(CheckoutFormModel model, string userId);
+	}
+}
---
+++ RestaurantApp.Core/Services/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using RestaurantApp.Core.Contracts;
+using RestaurantApp.Core.Models.ShoppingCart;
+using RestaurantApp.Data;
+using RestaurantApp.Infrastructure.Data.Models;
+
+namespace RestaurantApp.Core.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public OrderService(ApplicationDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task CheckoutAsync(CheckoutFormModel model, string userId)
+		{
+			var orderItems = new List<OrderItem>();
+
+			var order = new Order()
+			{
+				UserId = Guid.Parse(userId),
+				TotalPrice = model.TotalPrice,
+				OrderDate = DateTime.UtcNow,
+			};
+
+			foreach (var item in model.Items)
+			{
+				orderItems.Add(new OrderItem()
+				{
+					OrderId = order.Id,
+					ProductId = item.ProductId,
+					Quantity = item.Quantity,
+					Price = item.Price,
+				});
+			}
+
+			order.OrderItems = orderItems;
+
+			await dbContext.Orders.AddAsync(order);
+			await dbContext.SaveChangesAsync();
+		}
+	}
+}
