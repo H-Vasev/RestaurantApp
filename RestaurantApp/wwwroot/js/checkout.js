e8db2acb112121aefb7120f217d2b0798fc8fb4a
H.Vasev <hristovasev999@gmail.com>
2024-03-16 19:40:39 +0000

    Add checkout view and script files

---
+++ RestaurantApp/wwwroot/js/checkout.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    let initialDeliveryCharge = parseFloat($('#deliveryOptions').val());
+    let initialPrice = totalPrice + initialDeliveryCharge;
+
+    $('#totalPrice').text('€ ' + initialPrice.toFixed(2));
+    $('input[name="totalPrice"]').val(initialPrice);
+
+    $('#deliveryOptions').change(function () {
+        let charge = parseFloat($(this).val());
+        let newTotalPrice = totalPrice + charge;
+
+        $('#totalPrice').text('€ ' + newTotalPrice.toFixed(2));
+        $('input[name="totalPrice"]').val(newTotalPrice);
+    });
+});
