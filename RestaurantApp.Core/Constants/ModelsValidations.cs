20eb72f19210e9bc92c308cfd94f19d9e9628e17
H.Vasev <hristovasev999@gmail.com>
2024-03-16 19:38:58 +0000

    Update checkoutFormModel and add validation

---
+++ RestaurantApp.Core/Constants/ModelsValidations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Core.Constants
+{
+	public static class ModelsValidations
+	{
+		public static class CheckoutFormModel
+		{
+			public const int NameMaxLenght = 50;
+			public const int NameMinLenght = 3;
+
+			public const int AddressMaxLenght = 100;
+			public const int AddressMinLenght = 3;
+
+			public const int PostalCodeMaxLenght = 10;
+			public const int PostalCodeMinLenght = 4;
+
+			public const int EmailMaxLenght = 20;
+			public const int EmailMinLenght = 4;
+		}
+	}
+}
--- RestaurantApp.Core/Models/ShoppingCart/CheckoutFormModel.cs
+++ RestaurantApp.Core/Models/ShoppingCart/CheckoutFormModel.cs
@@ -1,10 +1,33 @@
-﻿namespace RestaurantApp.Core.Models.ShoppingCart
+﻿using System.ComponentModel.DataAnnotations;
+using static RestaurantApp.Core.Constants.ModelsValidations.CheckoutFormModel;
+
+namespace RestaurantApp.Core.Models.ShoppingCart
 {
 	public class CheckoutFormModel
 	{
+		[Required(ErrorMessage = "Name is required")]
+		[StringLength(NameMaxLenght, MinimumLength = NameMinLenght)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Address is required")]
+		[StringLength(AddressMaxLenght, MinimumLength = AddressMinLenght)]
+		public string Address { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Postal code is required")]
+		[StringLength(PostalCodeMaxLenght, MinimumLength = PostalCodeMinLenght)]
+		public string PostalCode { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Email is required")]
+		[StringLength(EmailMaxLenght, MinimumLength = EmailMinLenght)]
+		[EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+		[Phone]
+        public string? PhoneNumber { get; set; }
+
         public decimal TotalPrice { get; set; }
 
-		public ICollection<ShoppingCartViewModel> Items { get; set; } = new List<ShoppingCartViewModel>();
+		public IEnumerable<ShoppingCartViewModel> Items { get; set; } = new List<ShoppingCartViewModel>();
 
 	}
 }
