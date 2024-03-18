1ce3ba7c46bb52e4663541a35393cd329a653ab3
H.Vasev <hristovasev999@gmail.com>
2024-03-18 17:57:08 +0000

    Contact form implemented

---
+++ RestaurantApp/Controllers/ContactController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantApp.Core.Contracts;
+using RestaurantApp.Core.Models.Contact;
+
+namespace RestaurantApp.Controllers
+{
+    public class ContactController : BaseController
+    {
+        private readonly IEmailSender emailSender;
+
+        public ContactController(IEmailSender emailSender)
+        {
+			this.emailSender = emailSender;
+		}
+
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            var model = new ContactFormModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> SendContactForm(ContactFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+				return View(model);
+			}
+
+            try
+            {
+				await emailSender.SendEmailAsync(model.Name, model.Email, model.Subject, model.Message);
+			}
+			catch (Exception)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction(nameof(Index));
+		}
+    }
+}
