f14eceefc3576fb5f322aabdda1f647ff4cf01b8
H.Vasev <hristovasev999@gmail.com>
2024-03-30 16:55:30 +0000

    Add js notifications

---
+++ RestaurantApp/wwwroot/js/cancel-notification.js
@@ -0,0 +1,3 @@
+﻿function cancelReservation(date) {
+	return confirm('Are you sure you want to cancel the reservation for ' + date + '?');
+}
---
+++ RestaurantApp/wwwroot/js/show-notification.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    setTimeout(function () {
+        $('#dateMessage').fadeOut('slow', function () {
+            $(this).remove();
+        })
+    }, 3000);
+});
